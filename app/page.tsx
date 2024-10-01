e6de61b0e0afeff98dd7af65951401bf0aaa9886
Emeka Eric <emekaeric12@gmail.com>
2024-10-01 06:42:15 +0100

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Backdrop from "@/components/Backdrop/Backdrop";
 import Base from "@/components/ScreenA/Base";
 
 export default function page() {
   return (
     <div className="overflow-y-auto h-full">
       <Base />
+      <Backdrop />
     </div>
   )
 }
