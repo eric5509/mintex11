d21d798e6fb115c42f66f118d1ce405f8d896715
Emeka Eric <emekaeric12@gmail.com>
2024-10-02 10:26:08 +0100

    Implemented Backdrop Toggle

--- components/Layout/Main.tsx
+++ components/Layout/Main.tsx
@@ -1,8 +1,10 @@
 'use client'
+import { motion } from 'framer-motion'
 import { useAppSelector } from "@/redux/store/hook";
 import Body from "./Body";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
+import { variant } from '@/lib/framer';
 
 type Props = {
     children: React.ReactNode
@@ -12,11 +14,11 @@ export default function Main({ children }: Props) {
     const showSidebar = useAppSelector(store => store.sidebar.show)
     const type = useAppSelector(store => store.sidebar.type)
     return (
-        <div className="h-[calc(100vh-80px)] p-3 pb-0 overflow-y-auto">
+        <motion.div variants={variant} initial="initial" animate="animate" className="h-[calc(100vh-80px)] p-3 pb-0 overflow-y-auto">
             <div className={`h-full overflow-y-auto grid ${type !== "" ? "grid-cols-1fr lg:grid-cols-[310px_1fr] xl:grid-cols-[310px_1fr_346px] " : "grid-cols-1 lg:grid-cols-[310px_1fr] "} gap-3`}>
                 <LeftSidebar />
                 <Body children={children} />
                 <RightSidebar />
             </div>
-        </div>)
+        </motion.div>)
 }
--- components/ScreenD/Details.tsx
+++ components/ScreenD/Details.tsx
@@ -4,7 +4,7 @@ import Info from "./Info";
 
 export default function Details() {
     return (
-        <div className="p-3 bg-[#323434] border border-[#8996A9] rounded-xl rounded-tl-none text-white">
+        <div className="p-3 bg-[#323434] border-[#8996A9] rounded-xl rounded-tl-none text-white">
             <p className="text-xs text-[#ECEDEE]">Space Man (#Space) is a forward-thinking cryptocurrency project inspired by the pioneering spirit of space exploration and the legacy of human ingenuity in the cosmos. The project draws its core inspiration from the real-life achievements of astronauts and space pioneers who have ventured beyond Earth’s atmosphere to explore the unknown. By embodying the ethos of these explorers, Space Man seeks to push the boundaries of traditional finance and bring the vast potential of blockchain technology to new frontiers.             </p>
             <div className="grid grid-cols-2 mt-3 gap-5">
                 <Info img="curve.png" title="Bonding Curve"/>
