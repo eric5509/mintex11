4ac0e64fe7a86f8cefed5b731e67667550d50fef
Emeka Eric <emekaeric12@gmail.com>
2024-10-02 05:47:54 +0100

    Implemented Backdrop Toggle

--- components/Layout/LeftSidebar.tsx
+++ components/Layout/LeftSidebar.tsx
@@ -1,5 +1,8 @@
 'use client'
-
+import { IoMdHome } from "react-icons/io";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
+import { FaRegGem } from "react-icons/fa";
 import { useAppSelector } from "@/redux/store/hook"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
@@ -8,20 +11,22 @@ export default function LeftSidebar() {
   const darkmode = useAppSelector(store => store.darkmode.value)
   const pathname = usePathname()
   const links = [
-    ['Home', 'home.png', '/', 'home.png'],
-    ['All Tokens', 'tokens.png', '/tokens', 'bluetoken.png'],
-    ['Profile', 'profile.png', '/profile', 'blueprofile.png'],
-    ['Earn', 'gem1.png', '/earn', 'greengem.png'],
+    ['Home', 'home.png', '/', <IoMdHome  size={23}/>],
+    ['All Tokens', 'tokens.png', '/tokens', <BsFillGridFill style={{rotate: '45deg'}}  size={18}/>],
+    ['Profile', 'profile.png', '/profile', <FaUserAlt  size={18}/>],
+    ['Earn', 'gem1.png', '/earn', <FaRegGem size={19}/>],
   ]
   return (
     <div className={`border-2 border-[#862078] flex-col hidden lg:flex justify-between ${darkmode ? "bg-[#210716]": ""} duration-500 transition-colors h-full p-3 rounded-2xl`}>
       <div className="flex flex-col gap-3">
         {links.map((el, key) => (
-          <Link key={key} href={`${el[2]}`} className={`flex items-center border-2 cursor-pointer border-transparent hover:border-prim duration-500 shadow-md transition-colors ${pathname === el[2] ? "bg-[#9DFBFA] text-secondary font-bold" : `${darkmode ? "bg-[#242727] text-[#EBEBEB]": "bg-white text-black"}`} rounded-xl gap-3 `}>
+          <Link key={key} href={`${el[2]}`} className={`flex group items-center border-2 cursor-pointer border-transparent hover:border-prim duration-500 shadow-md transition-colors ${pathname === el[2] ? "bg-[#9DFBFA] text-secondary font-bold" : `${darkmode ? "bg-[#242727] text-[#EBEBEB]": "bg-white text-black"}`} rounded-xl gap-[2px] `}>
             <div className="h-[50px] w-12 rounded-md grid place-content-center">
-              <img src={darkmode ? el[1] : el[3]} alt="" />
+              <span className={` duration-300 ${pathname != el[2] && "group-hover:text-prim group-hover:scale-105"}`}>
+                {el[3]}
+              </span>
             </div>
-            <p className="">{el[0]}</p>
+            <p className={`text-sm duration-300 ${pathname != el[2] && "group-hover:text-prim group-hover:scale-105"}`}>{el[0]}</p>
           </Link>
         ))}
       </div>
