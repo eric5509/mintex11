0c53f0a28a2fca0cf7319ae376c66bc2409f205d
Emeka Eric <emekaeric12@gmail.com>
2024-10-02 05:49:54 +0100

    Implemented Backdrop Toggle

--- components/ScreenD/DisplayCommentCards.tsx
+++ components/ScreenD/DisplayCommentCards.tsx
@@ -2,7 +2,7 @@ import CommentCard from "./CommentCard";
 
 export default function DisplayCommentCards() {
   return (
-    <div className="flex flex-col relative border border-prim p-4 gap-4">
+    <div className="flex flex-col relative border rounded-b-2xl border-prim p-4 gap-4">
       <p className="p-3 rounded-xl absolute top-3 right-3 bg-prim text-secondary font-semibold">Post a Comment</p>
       {[1, 2, 3, 4].map((el, key) => (
         <div key={key} className="">
